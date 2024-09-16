5a3779de5f8a1ccd58d66a0f7730f03d3b2ec13c
Jimias <jimias@metropolia.fi>
2024-09-16 10:27:12 +0300

    eka versio

---
+++ maanosakoodit.py
@@ -0,0 +1,44 @@
+import mysql.connector
+
+
+
+'''
+Esimerkki yhteyden muodostamisesta ja yhteysolion viemisestä funktiolle
+'''
+
+
+def KyselyFunktio(yhteys:object)->list:
+    '''
+    Tehdään sql kysely ja palautetaan serveriltä saatu vastaus
+    '''
+
+    komento = "SELECT DISTINCT continent from airport;"  #sql komento
+    kursori = yhteys.cursor() #luodaan kursori
+
+    kursori.execute(komento) #suoritetaan komento
+    tulos = kursori.fetchall()  #haetaan kaikki tulokset
+
+    return [rivi[0] for rivi in tulos]
+
+
+
+if __name__ == '__main__': #pääohjelma
+
+
+    try:
+        yhteys = mysql.connector.connect(
+                 host='127.0.0.1',
+                 port= 3306,
+                 database='flight_game',
+                 user='jimias',
+                 password='dobronx',
+                 autocommit=True
+                 )
+    except mysql.connector.Error as err: #virhe yhteyden muodostamisessa
+        print(err)
+
+
+    else: #yhteys muodostettu onnistuneesti
+        maanosat = KyselyFunktio(yhteys)
+        for maanosakoodi in maanosat:
+            print(maanosakoodi)
