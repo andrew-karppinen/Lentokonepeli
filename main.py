bce1ff8d8238f535ef0cdaffba208bdcb1858370
ak <andrew1karppinen@gmail.com>
2024-09-20 21:05:29 +0300

    Päivitetty pääohjelmaa Lisätty tietokantaan yhdistämisvalikko Lisätty toiminto joka mahdollistaa asetusten tallentamisen tiedostoon jotta käyttäjän ei tarvitse näitä antaa uudestaan joka kerta

--- main.py
+++ main.py
@@ -5,20 +5,68 @@ from src import * #importataan funktiot
 
 
 
-#yhteys
-try:
-    yhteys = mysql.connector.connect(
-        host='127.0.0.1',
-        port= 3306,
-        database='flight_game',
-        user='sqlkayttaja',
-        password='salasana123',
-        autocommit=True)
-except mysql.connector.Error as err: # virhe yhteyden muodostamisessa
-    print(err)
+
+
+def yhdista():
+
+    '''
+    Käyttöliittymä serveriin yhdistämiseen
+
+    Jos yhdistäminen onnistuu, palautetaan yhteys
+    muuten palautetaan False
+
+    Mahdollista käyttää tiedostoon tallennettuja asetuksia
+
+    '''
+
+
+    while True:
+
+        asetukset = LueAsetukset() #yritys lukea asetukset tiedostosta
+
+        lue_asetukset = False
+
+        if asetukset != False: #asetukset löytyy tiedostosta
+            if input("serverin asetukset on tallennettu aikaisemmalla kerralla haluatko käyttää niitä (k/e): ").lower() == "k":
+                lue_asetukset = True
+                server_ip,kayttaja,salasana = asetukset
+
+
+        if lue_asetukset == False: #kysytään asetukset käyttäjältä
+            print("Anna sql palvelimen tiedot, (exit = poistu)")
+            server_ip = input("Anna serverin ip: ")
+            kayttaja = input("Anna käyttäjätunnus: ")
+            salasana = input("Anna salasana: ")
+
+
+        if "exit" in (server_ip,kayttaja,salasana): #jos käyttäjä haluaa poistua
+            return False
+
+
+        # yhteys
+        try:
+            yhteys = mysql.connector.connect(
+                host=server_ip,
+                port=3306,
+                database='flight_game',
+                user=kayttaja,
+                password=salasana,
+                autocommit=True)
+        except mysql.connector.Error as err:  # virhe yhteyden muodostamisessa
+            print("Virhe yhteyden muodostamisessa tietokantaan: ", err)
+            continue
+
+        else:
+            if lue_asetukset == False: #jos asetukset on luettu tiedostosta, ei kysytä uudestaan
+                if input("Yhteys muodostettu onnistuneesti, haluan tallentaa asetukset (k/e)").lower() == "k":
+                    TallennaAsetukset(server_ip,kayttaja,salasana)
+
+            return yhteys
 
 
 
+
+yhteys = yhdista()
 
 
 print("Maanosat: ", maanosakoodit(yhteys))
---
+++ src/asetukset.py
@@ -0,0 +1,67 @@
+import os
+
+
+import platform
+
+
+def TallennaAsetukset(server_ip, kayttaja, salasana):
+
+    '''
+    Tallentaa asetukset tiedostoon
+
+    '''
+
+    path = GetHomeConfigDirectory() + "/flight_game"
+
+    if not os.path.exists(path): #hakemistoa ei ole
+        os.makedirs(path) #luodaan hakemisto
+
+    with open(path + "/asetukset.txt", "w") as f:
+        f.write(f"{server_ip}\n{kayttaja}\n{salasana}")
+
+
+def LueAsetukset():
+        '''
+        Lukee asetukset tiedostosta
+
+        data = server_ip,kayttaja,salasana
+        '''
+
+        path = GetHomeConfigDirectory() + "/flight_game/asetukset.txt"
+
+        if not os.path.exists(path): #tiedostoa ei ole
+            return False
+
+        with open(path, "r") as f:
+            data = f.read().split("\n")
+
+        return data
+
+
+def GetHomeConfigDirectory():
+
+    '''
+    get this path in linux:
+    home/.config
+    
+    get this path in windows:
+    appdata/Roaming
+
+    '''
+
+    system = platform.system() #get operating system
+
+    if system == "Linux": #linux        
+        path = os.path.expanduser('~') + "/.config"
+
+        return path
+
+    elif system == "Windows": #windows
+        path = os.getenv('APPDATA')
+        return path
+
+if __name__ == "__main__":
+    
+    
+    print(GetHomeConfigDirectory())
+    
