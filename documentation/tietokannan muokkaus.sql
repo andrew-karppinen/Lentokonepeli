f17484651abe70cd78965a4e114e2f17f26dc003
ak <andrew1karppinen@gmail.com>
2024-10-07 15:02:05 +0300

    päivitetty documentatiota

--- documentation/tietokannan muokkaus.sql
+++ documentation/tietokannan muokkaus.sql
@@ -1,19 +1,29 @@
 
 
+-- Varmista että käyttäjällä tarvittavat oikeudet tietokantaan
 
 
+-- Poista taulut, jos ne ovat jo olemassa:
+SET FOREIGN_KEY_CHECKS = 0; -- vierasavainten tarkistukset pois päältä
+
+DROP TABLE goal
+DROP TABLE goal_reached
+DROP TABLE game
+
+
+
+-- luodaan uudet taulut:
 
 -- luodaan taulu user_information
 CREATE TABLE user_information (
     name VARCHAR(255) PRIMARY KEY
 );
 
-
+-- luodaan taulu game
 CREATE TABLE game (
     game_id INT AUTO_INCREMENT PRIMARY KEY,
     continent VARCHAR(255)
 );
-
 
 
 -- luodaan taulu user_games
@@ -27,6 +37,4 @@ CREATE TABLE user_games (
 
 
 
-
-
-
+SET FOREIGN_KEY_CHECKS = 1; -- vierasavainten tarkistukset takaisin päälle
