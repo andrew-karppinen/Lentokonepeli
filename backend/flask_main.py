d3508f2fb05d9d513b3737ad3853e0a99008c2e4
ak <awkk12345@gmail.com>
2024-11-22 10:50:05 +0200

    tehty flask pohja

---
+++ backend/flask_main.py
@@ -0,0 +1,67 @@
+
+from flask import Flask, request, jsonify
+import mysql.connector
+
+from backend.src.maanosakoodit import maanosakoodit
+from src import *
+
+#luodaan sql yhteys
+yhteys = mysql.connector.connect(
+    host="localhost",
+    port=3306,
+    database='flight_game',
+    user="sqlkayttaja",
+    password="salasana123",
+    connection_timeout=5,
+    autocommit=True)
+
+
+app = Flask(__name__)
+
+
+@app.route('/api/getcontinents', methods=['get'])
+def get_continents():
+    '''
+    Palauttaa maanosakoodit
+    '''
+    koodit = maanosakoodit(yhteys)  #hakee maanosakoodit listaan
+
+    vastaus = {
+        "continents": koodit
+    }
+    return jsonify(vastaus)
+
+
+@app.route('/api/getcountries', methods=['GET'])
+def get_countries():
+    '''
+    Palauttaa maanosan maat
+    '''
+    pass
+
+
+
+@app.route('/api/get-user-scores', methods=['GET'])
+def get_user_scores():
+    pass
+
+
+@app.route('/api/new-game', methods=['POST'])
+def new_game():
+    pass
+
+
+
+@app.route('/api/get-saved-game', methods=['GET']):
+def get_saved_game():
+    pass
+
+@app.route('/api/save-game', methods=['POST']):
+def save_game():
+    pass
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
