2a6906aa0daf0b0c64938a4fd8968d180c5450f0
ak <awkk12345@gmail.com>
2024-12-02 13:24:17 +0200

    tehty uudensipeli sivua eteenpäin

--- static/uusipeli.js
+++ static/uusipeli.js
@@ -1,6 +1,6 @@
 'use strict';
 
-
+//haetaan pelaajien nimi elementit
 const player1 = document.getElementById('player1');
 const player2 = document.getElementById('player2');
 const player3 = document.getElementById('player3');
@@ -8,21 +8,67 @@ const player4 = document.getElementById('player4');
 const player5 = document.getElementById('player5');
 const player6 = document.getElementById('player6');
 
-const pelaajat = [player1,player2,player3,player4,player5,player6];
+const pelaajat = [player1,player2,player3,player4,player5,player6]; //laitetaan ne listaan, elämän helpottamiseksi
 
-
+//haetaan aloita ja peruuta nappi
 const aloita_nappi = document.getElementById('aloita_nappi')
 const peruuta_nappi = document.getElementById('peruuta_nappi')
 
+//haetaan maanosa ja maa listavalikko elementit
+const maa_lista = document.getElementById('maa');
+const maanosa_lista = document.getElementById('maanosa');
 
-let maa = "suomi";
-let maanosa = "eurooppa";
 
 
-let pelin_tiedot = { 'gamedata':{'country': maa, 'continent': maanosa, 'players':[]}};
+function paivita_maat(maanosa='*')
+{
+    let endpoint = '/api/getcountries'+'?continent='+maanosa;
+
+    //haetaan maat /api/getcountries
+    fetch(endpoint)
+        .then(function(response) {
+            if (!response.ok) {
+                throw new Error('error');
+            }
+            return response.json();
+        })
+        .then(function(data) {
+            maa_lista.innerHTML = ""; //tyhjennetään taulukko
+
+            let option = document.createElement('option'); //lisätään * valinta
+            option.value = '*'; //value on * ja teksti on *
+            option.text = '*'; //value on * ja teksti on *
+
+            maa_lista.appendChild(option);
+            for (let i of data['countries']) { //käydään palvelimelta saadut maat läpi
+                let option = document.createElement('option');
+                option.value = i;
+                option.text = i;
+                maa_lista.appendChild(option); // Append to maa_lista instead of maanosa_lista
+            }
+        })
+        .catch(function(error) {
+            console.log(error);
+        });
+
+}
+
+
+paivita_maat(); //päivitetään maat kertaalleen
+
+
+maanosa_lista.addEventListener('change', function() { //jos maanosalistaa muutetaan, päivitetään maat
+    paivita_maat(maanosa_lista.value)
+});
+
+
 
 
 aloita_nappi.addEventListener('click', function() {
+
+    console.log(maa_lista.value, maanosa_lista.value);
+
+    let pelin_tiedot = {'gamedata':{'country': maa_lista.value, 'continent': maa_lista.value, 'players':[]}};
 
 
 
@@ -57,7 +103,7 @@ aloita_nappi.addEventListener('click', function() {
                     console.log("tässä");
                     throw new Error('error'); //aiheuttaa virheen
                 }
-                return response.json(); //palauttaa promise jos ei tule erroria
+                return response.json(); //palauttaa jsonin jos ei tule erroria
             })
             .then(function(data) {
                 console.log(data);
